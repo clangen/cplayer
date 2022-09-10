e11de56b6e33157b822cdfca4509bb586249b59a
casey langen <casey.langen@gmail.com>
2022-09-09 20:08:24 -0700

    Update for DEV mode. Soon we'll fetch the manifest asynchronously.

--- src/AlbumManifest.tsx
+++ src/AlbumManifest.tsx
@@ -1,26 +1,30 @@
+const PATH_PREFIX = import.meta.env.DEV
+  ? "/src/assets/albums"
+  : "./assets/albums";
+
 const Hustle = {
   name: "the beachside hustle",
   artist: "suncrash",
   tracks: [
     {
       title: "crosstown throwdown",
-      uri: "/src/assets/albums/the_beachside_hustle/01-crosstown_throwdown.opus",
+      uri: `${PATH_PREFIX}/the_beachside_hustle/01-crosstown_throwdown.opus`,
     },
     {
       title: "oleander",
-      uri: "/src/assets/albums/the_beachside_hustle/02-oleander.opus",
+      uri: `${PATH_PREFIX}/the_beachside_hustle/02-oleander.opus`,
     },
     {
       title: "over/under",
-      uri: "/src/assets/albums/the_beachside_hustle/03-over_under.opus",
+      uri: `${PATH_PREFIX}/the_beachside_hustle/03-over_under.opus`,
     },
     {
       title: "road to santa fe",
-      uri: "/src/assets/albums/the_beachside_hustle/04-road_to_santa_fe.opus",
+      uri: `${PATH_PREFIX}/the_beachside_hustle/04-road_to_santa_fe.opus`,
     },
     {
       title: "up empty",
-      uri: "/src/assets/albums/the_beachside_hustle/05-up_empty.opus",
+      uri: `${PATH_PREFIX}/the_beachside_hustle/05-up_empty.opus`,
     },
   ],
 };
@@ -31,59 +35,59 @@ const Disaster = {
   tracks: [
     {
       title: "rise and shine",
-      uri: "/src/assets/albums/immediate_disaster/01-rise_and_shine.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/01-rise_and_shine.opus`,
     },
     {
       title: "listless days",
-      uri: "/src/assets/albums/immediate_disaster/02-listless_days.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/02-listless_days.opus`,
     },
     {
       title: "heads up",
-      uri: "/src/assets/albums/immediate_disaster/03-heads_up.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/03-heads_up.opus`,
     },
     {
       title: "backroads",
-      uri: "/src/assets/albums/immediate_disaster/04-backroads.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/04-backroads.opus`,
     },
     {
       title: "flyback",
-      uri: "/src/assets/albums/immediate_disaster/05-flyback.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/05-flyback.opus`,
     },
     {
       title: "downtown slalom",
-      uri: "/src/assets/albums/immediate_disaster/06-downtown_slalom.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/06-downtown_slalom.opus`,
     },
     {
       title: "surf dirty",
-      uri: "/src/assets/albums/immediate_disaster/07-surf_dirty.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/07-surf_dirty.opus`,
     },
     {
       title: "southbound blues",
-      uri: "/src/assets/albums/immediate_disaster/08-southbound_blues.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/08-southbound_blues.opus`,
     },
     {
       title: "insert coin",
-      uri: "/src/assets/albums/immediate_disaster/09-insert_coin.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/09-insert_coin.opus`,
     },
     {
       title: "summer's purge",
-      uri: "/src/assets/albums/immediate_disaster/10-summers_purge.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/10-summers_purge.opus`,
     },
     {
       title: "send help",
-      uri: "/src/assets/albums/immediate_disaster/11-send_help.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/11-send_help.opus`,
     },
     {
       title: "roundabout",
-      uri: "/src/assets/albums/immediate_disaster/12-roundabout.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/12-roundabout.opus`,
     },
     {
       title: "2am tumble",
-      uri: "/src/assets/albums/immediate_disaster/13-2am_tumble.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/13-2am_tumble.opus`,
     },
     {
       title: "signoff",
-      uri: "/src/assets/albums/immediate_disaster/99-signoff.opus",
+      uri: `${PATH_PREFIX}/immediate_disaster/99-signoff.opus`,
     },
   ],
 };
